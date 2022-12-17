bf3423cc8cc771a0393993c4066515a516f48765
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-16 20:14:40 -0800

    added methods to db class

--- db/index.js
+++ db/index.js
@@ -14,4 +14,30 @@ db.connect(function (err) {
 	if (err) throw err
 })
 
-module.exports = db
+class Database {
+	constructor(db) {
+		this.db = db
+	}
+
+	fetchEmployees() {
+		return this.db
+			.promise()
+			.query(
+				"Select employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+			)
+	}
+
+	fetchRolls() {
+		return this.db
+			.promise()
+			.query(
+				'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;'
+			)
+	}
+
+	addEmployee(employee) {
+		return this.db.promise().query('INSERT INTO employee set ?', employee)
+	}
+}
+
+module.exports = Database
