4b474b8f6b081bd132e514acd55b9412266f8841
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-16 22:37:09 -0800

    put db connection in seperate module

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		user: 'root',
+		password: process.env.PASSWORD,
+		database: 'employees_db',
+	},
+	console.log('Connected to employees_db')
+)
+db.connect(function (err) {
+	if (err) throw err
+})
+module.exports = db
